02e76f9c0a3397a781ffba3e576441925f252526
play4fun <play4fun@foxmail.com>
2017-12-06 10:12:39 +0800

    config

---
+++ src/config.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2017/12/6 10:11
+# @Author  : play4fun
+# @File    : config.py
+# @Software: PyCharm
+
+"""
+config.py:
+"""
+
+
+#百度api
+
+
+
+#微软bing
+
+#有道翻译api
